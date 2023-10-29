e6fc05469ad5cf3ba45985db6f031280c907acb4
El Filaly <elfilalyabdeljalil@gmail.com>
2023-10-29 17:32:00 +0100

    Change coding style of script.js|style.css files

--- script.js
+++ script.js
@@ -53,49 +53,50 @@ const user_default = user_pick.querySelector("#default");
 
 btn_edit_img.addEventListener("click", () => {
   hidden_file.click();
-function drog_drop_function(){
-    let lists_story_user=document.getElementsByClassName('story');
-    let todo_box=document.getElementById('todo_box');
-    let doing_box=document.getElementById('doing_box');
-    let finish_box=document.getElementById('finish_box');
-    for(list_story of lists_story_user){
-        list_story.addEventListener('dragstart', function(e){
-            let selected = e.target;
-            todo_box.addEventListener('dragover', function(e){
-                e.preventDefault();
-            })
-            todo_box.addEventListener('drop', function(e){
-                todo_box.appendChild(selected);
-                selected=null;
-            })
-            doing_box.addEventListener('dragover', function(e){
-                e.preventDefault();
-            })
-            doing_box.addEventListener('drop', function(e){
-                doing_box.appendChild(selected);
-                selected=null;
-            })
-            finish_box.addEventListener('dragover', function(e){
-                e.preventDefault();
-            })
-            finish_box.addEventListener('drop', function(e){
-                finish_box.appendChild(selected);
-                selected=null;
-            })
-        });         
+  function drog_drop_function() {
+    let lists_story_user = document.getElementsByClassName("story");
+    let todo_box = document.getElementById("todo_box");
+    let doing_box = document.getElementById("doing_box");
+    let finish_box = document.getElementById("finish_box");
+    for (list_story of lists_story_user) {
+      list_story.addEventListener("dragstart", function (e) {
+        let selected = e.target;
+        todo_box.addEventListener("dragover", function (e) {
+          e.preventDefault();
+        });
+        todo_box.addEventListener("drop", function (e) {
+          todo_box.appendChild(selected);
+          selected = null;
+        });
+        doing_box.addEventListener("dragover", function (e) {
+          e.preventDefault();
+        });
+        doing_box.addEventListener("drop", function (e) {
+          doing_box.appendChild(selected);
+          selected = null;
+        });
+        finish_box.addEventListener("dragover", function (e) {
+          e.preventDefault();
+        });
+        finish_box.addEventListener("drop", function (e) {
+          finish_box.appendChild(selected);
+          selected = null;
+        });
+      });
     }
+  }
+});
+function affiche() {
+  var menu_vertical = document.getElementById("header_vertical");
+  var filter = document.getElementById("filter");
+  filter.classList.remove("d-none");
+  menu_vertical.classList.add("header_vertical_mobile");
 }
-function affiche(){
-    var menu_vertical = document.getElementById('header_vertical');
-    var filter = document.getElementById('filter');
-    filter.classList.remove('d-none')
-    menu_vertical.classList.add("header_vertical_mobile");
-}
-function close_menu(){
-    var menu_vertical = document.getElementById('header_vertical');
-    var filter = document.getElementById('filter');
-    filter.classList.add('d-none')
-    menu_vertical.classList.remove("header_vertical_mobile");
+function close_menu() {
+  var menu_vertical = document.getElementById("header_vertical");
+  var filter = document.getElementById("filter");
+  filter.classList.add("d-none");
+  menu_vertical.classList.remove("header_vertical_mobile");
 }
 
 //****page parametre
