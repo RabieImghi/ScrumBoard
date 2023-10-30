b67a0ddc333e9e02284804147c60934d7fe5e66a
El Filaly <elfilalyabdeljalil@gmail.com>
2023-10-30 11:53:06 +0100

    update affichage image on Js file

--- script.js
+++ script.js
@@ -43,49 +43,6 @@ function close_menu() {
   filter.classList.add("d-none");
   menu_vertical.classList.remove("header_vertical_mobile");
 }
-// ------------PARTIE filaly*do not touch it if ur not filaly*
-const btn_edit_img = document.getElementById("editPicture");
-const hidden_file = document.getElementById("hiddenFileInput");
-const user_pick = document.getElementById("user_pick");
-const user = document.getElementsByClassName("user");
-const user_elements = user_pick.querySelectorAll("li");
-const user_default = user_pick.querySelector("#default");
-
-btn_edit_img.addEventListener("click", () => {
-  hidden_file.click();
-  function drog_drop_function() {
-    let lists_story_user = document.getElementsByClassName("story");
-    let todo_box = document.getElementById("todo_box");
-    let doing_box = document.getElementById("doing_box");
-    let finish_box = document.getElementById("finish_box");
-    for (list_story of lists_story_user) {
-      list_story.addEventListener("dragstart", function (e) {
-        let selected = e.target;
-        todo_box.addEventListener("dragover", function (e) {
-          e.preventDefault();
-        });
-        todo_box.addEventListener("drop", function (e) {
-          todo_box.appendChild(selected);
-          selected = null;
-        });
-        doing_box.addEventListener("dragover", function (e) {
-          e.preventDefault();
-        });
-        doing_box.addEventListener("drop", function (e) {
-          doing_box.appendChild(selected);
-          selected = null;
-        });
-        finish_box.addEventListener("dragover", function (e) {
-          e.preventDefault();
-        });
-        finish_box.addEventListener("drop", function (e) {
-          finish_box.appendChild(selected);
-          selected = null;
-        });
-      });
-    }
-  }
-});
 function affiche() {
   var menu_vertical = document.getElementById("header_vertical");
   var filter = document.getElementById("filter");
@@ -98,12 +55,24 @@ function close_menu() {
   filter.classList.add("d-none");
   menu_vertical.classList.remove("header_vertical_mobile");
 }
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+var myModal = document.getElementById("myModal");
+var myInput = document.getElementById("myInput");
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+myModal.addEventListener("shown.bs.modal", function () {
+  myInput.focus();
+});
+
+// ------------PARTIE filaly*do not touch it if ur not filaly*
+const btn_edit_img = document.getElementById("editPicture");
+const hidden_file = document.getElementById("hiddenFileInput");
+const user_pick = document.getElementById("user_pick");
+const user = document.getElementsByClassName("user");
+const user_elements = user_pick.querySelectorAll("li");
+const user_default = user_pick.querySelector("#default");
+
+btn_edit_img.addEventListener("click", () => {
+  hidden_file.click();
+});
 
 //****page parametre
 const editPictureButton = document.getElementById("editPicture");
