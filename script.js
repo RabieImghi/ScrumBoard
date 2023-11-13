8afe969297289487b3920739d955b5aef2ed2f2f
RabieImghi <rabieaitimghi7@gmail.com>
2023-11-13 10:40:21 +0100

    update drog and drop function en scrum board

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function drog_drop_function() {
   let todo_box = document.getElementById("todo_box");
   let doing_box = document.getElementById("doing_box");
   let finish_box = document.getElementById("finish_box");
-  for (list_story of lists_story_user) {
+  for (list_story of lists_story_user) { // for (int i=0; i<lists_story_user.lenght; i++){ lists_story_user[i] }
     list_story.addEventListener("dragstart", function (e) {
       let selected = e.target;
       todo_box.addEventListener("dragover", function (e) {
